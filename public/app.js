ab2fb1bb87fde8112b4eb104673ddca9e432543c
kvt500 <kvt500@gmail.com>
2023-03-02 11:35:13 +0500

    orders button

--- public/app.js
+++ public/app.js
@@ -17,6 +17,7 @@ function add_to_cart(id)
 	//alert('Items in your cart: ' + cart_get_number_of_items());
 	//alert('1');
 	update_orders_input();
+	update_orders_button();
 }
 
 
@@ -61,4 +62,10 @@ function update_orders_input()
 
 	var orders = cart_get_orders();
 	$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+	var text  = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
 }
